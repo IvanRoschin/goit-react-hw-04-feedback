b5f70ee70379d9bb8b550f75c3fe2603b15f7acf
Ivan Roschin <ivan.roschin86@gmail.com>
2022-10-26 20:12:47 +0300

    add section

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FeedbackButtons } from './FeedbackButtons';
-
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Section } from 'components/Section/Section';
 export class Feedback extends React.Component {
   static defaulProps = {
     initialValue: 0,
@@ -9,68 +10,32 @@ export class Feedback extends React.Component {
   //   static PropTypes = {};
 
   state = {
-    goodValue: this.props.initialValue,
-    neutralValue: this.props.initialValue,
-    badValue: this.props.initialValue,
+    good: this.props.initialValue,
+    neutral: this.props.initialValue,
+    bad: this.props.initialValue,
   };
 
-  goodIncrement = () => {
+  increment = event => {
     this.setState(prevState => {
       return {
-        goodValue: prevState.goodValue + 1,
+        [event.target.name]: prevState[event.target.name] + 1,
       };
     });
   };
-
-  neutralIncrement = () => {
-    this.setState(prevState => {
-      return {
-        neutralValue: prevState.neutralValue + 1,
-      };
-    });
-  };
-
-  badIncrement = () => {
-    this.setState(prevState => {
-      return {
-        badValue: prevState.badValue + 1,
-      };
-    });
-  };
-
-  //   countTotalFeedback = () => {
-  //     this.setState(prevState => {
-  //       return this.goodIncrement + this.neutralIncrement + this.badIncrement;
-  //     });
-  //   };
 
   render() {
     return (
       <div>
-        Feedback
-        <h2>Please leave feedback</h2>
-        neutralIncrement,
-        <FeedbackButtons
-          goodIncrement={this.goodIncrement}
-          neutralIncrement={this.neutralIncrement}
-          badIncrement={this.badIncrement}
-        />
-        <h2>Statistics</h2>
-        <div>
-          Good: <span>{this.state.goodValue}</span>
-        </div>
-        <div>
-          Neutral: <span>{this.state.neutralValue}</span>
-        </div>
-        <div>
-          Bad: <span>{this.state.badValue}</span>
-        </div>
-        <div>
-          Total: <span>{this.countTotalFeedback}</span>
-        </div>
-        <div>
-          Positive feedback: <span>{this.state.badValue}</span>
-        </div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions increment={this.increment} />
+        </Section>
+        <Section>
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+          />
+        </Section>
       </div>
     );
   }
